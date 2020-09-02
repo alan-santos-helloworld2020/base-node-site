2de5eef73cd6ffe4df2ba0652e504ca3d5d83fe9
alan-santos-helloworld2020 <alanprotesto@gmail.com>
2020-09-02 19:41:11 -0300

    formatado

--- router/router.js
+++ router/router.js
@@ -9,10 +9,10 @@ router.use(bodyParser.json());
 router.use(express.static(path.join(__dirname,'..','public')));
 
 
-router.get('/',(req, res) => res.sendFile(path.join(__dirname,'../','views/index.html')));
-router.get('/produtos',(req, res) => res.sendFile(path.join(__dirname,'../','views/produtos.html')));
-router.get('/cadastro',(req, res) => res.sendFile(path.join(__dirname,'../','views/cadastro.html')));
-router.get('/sobre',(req, res) => res.sendFile(path.join(__dirname,'../','views/sobre.html')));
+router.get('/',(req, res) => res.sendFile(path.join(__dirname,'../','views','index.html')));
+router.get('/produtos',(req, res) => res.sendFile(path.join(__dirname,'../','views','produtos.html')));
+router.get('/cadastro',(req, res) => res.sendFile(path.join(__dirname,'../','views','cadastro.html')));
+router.get('/sobre',(req, res) => res.sendFile(path.join(__dirname,'../','views','sobre.html')));
 
 
 // var caminho = path.join(__dirname,'../','views');
